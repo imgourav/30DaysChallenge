6eada3cccc8b397d7e0e2ab4783eb071cb33061e
Gourav Sharma <gs3150007@gmail.com>
2021-10-02 22:25:49 +0530

    Create Day28_Text to Handwritten.py

---
+++ Day28_Text to Handwritten.py
@@ -0,0 +1,4 @@
+import pywhatkit
+pywhatkit.text_to_handwriting("""Coding is a skill where you take instructions (the steps in a task) and translate it 
+  into a language the computer understands since computers do not communicate like humans. They communicate in a 
+  language called BINARY and it is uses 0's and 1's. Coders write the instructions using a programming language.""",rgb=(0,0,255))
